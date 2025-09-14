a16c2387f55a6ddd267f66340afaf00d8ac65514
04sharadkumar <04sharadkumar@gmail.com>
2025-09-14 17:46:41 +0530

    app jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,37 +3,43 @@ import PlacementEssentials from "./pages/PlacementEssentials";
 import TopicPage from "./pages/TopicPage";
 import PlacementHome from "./pages/PlacementHome";
 import QuizPage from "./pages/QuizPage";
-
+import NotFound from "./pages/NotFound"; // optional
 
 function App() {
   return (
     <Routes>
-
-      
-
       {/* Home page */}
       <Route path="/" element={<PlacementHome />} />
 
-      {/* Placement Essentials with optional params */}
+      {/* Main placement page */}
       <Route path="/placementEssentials" element={<PlacementEssentials />} />
 
-
+      {/* Sub-tab page */}
       <Route
         path="/placementEssentials/:tabName/:subTabName"
         element={<PlacementEssentials />}
       />
 
-      {/* Topic Page inside Placement Essentials */}
+      {/* Topic page */}
       <Route
         path="/placementEssentials/:tabName/:subTabName/:topicName"
         element={<TopicPage />}
       />
 
+      {/* ✅ Quiz page with /questions */}
       <Route
-      path="/placementEssentials/:tabName/:subTabName/:topicName/quizQuestion"
-      element={<QuizPage />}
+        path="/placementEssentials/:tabName/:subTabName/:topicName/questions"
+        element={<QuizPage />}
       />
 
+      {/* (Optional) Quiz page with /quizQuestion (if you still use it) */}
+      <Route
+        path="/placementEssentials/:tabName/:subTabName/:topicName/quizQuestion"
+        element={<QuizPage />}
+      />
+
+      {/* Optional 404 page */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
